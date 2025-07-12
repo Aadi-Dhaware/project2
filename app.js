6fe8732b36f326ef6d542d21e9a9c740fcc88cd8
Aditya Dhaware <dhawareaditya201@gmail.com>
2025-07-12 21:05:40 +0530

    backend-demo-code

---
+++ app.js
@@ -0,0 +1,11 @@
+// added new backend code 
+const express = require("express");
+const Mongoose = require("mongoose");
+const app = express();
+const port = 3000;
+const MongooseURL = "mongodb://localhost:27017/mydatabase";
+
+app.listen(port, () => {
+  Mongoose.connect(MongooseURL);
+  console.log(`Server is running on port ${port}`);
+});
