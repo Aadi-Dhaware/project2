d2d4e82f42986f32ee51d0a237ffb2af2fc8b39e
Aditya Dhaware <dhawareaditya201@gmail.com>
2025-07-14 21:43:33 +0530

    home-route

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const app = express();
 const port = 3000;
 const MongooseURL = "mongodb://localhost:27017/mydatabase";
 
-app.listen(port, () => {
-  Mongoose.connect(MongooseURL);
-  console.log(`Server is running on port ${port}`);
+app.get("/home", (req, res) => {
+  res.send("Home Page");
 });
 
 app.get("/about", (req, res) => {
@@ -21,4 +20,9 @@ app.get("/contact", (req, res) => {
 
 app.post("/submit", (req, res) => {
   res.send("Form submitted successfully");
+});
+
+app.listen(port, () => {
+  Mongoose.connect(MongooseURL);
+  console.log(`Server is running on port ${port}`);
 });
