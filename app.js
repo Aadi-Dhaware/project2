4277a1e801a6c8571d29eb2d220bddcff85c8e9a
Aditya Dhaware <dhawareaditya201@gmail.com>
2025-07-20 00:51:10 +0530

    Initail commit

--- app.js
+++ app.js
@@ -6,25 +6,3 @@ const app = express();
 const port = 3000;
 const MongooseURL = "mongodb://localhost:27017/mydatabase";
 
-//home-route
-app.get("/home", (req, res) => {
-  res.send("Home Page");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About Page");
-});
-
-app.get("/contact", (req, res) => {
-  res.send("Contact Page");
-});
-
-app.post("/submit", (req, res) => {
-  res.send("Form submitted successfully");
-});
-
-
-app.listen(port, () => {
-  Mongoose.connect(MongooseURL);
-  console.log(`Server is running on port ${port}`);
-});
