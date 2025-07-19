dbbc076fc3173f2e9a5ed9bca37e5d64eb26b3ee
Aditya Dhaware <dhawareaditya201@gmail.com>
2025-07-20 01:39:35 +0530

    feature-added

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ const app = express();
 const port = 3000;
 const MongooseURL = "mongodb://localhost:27017/mydatabase";
 
+app.get("/feature", (req, res) => {
+  res.send("On feature");
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Listening on port ${port}`);
+});
